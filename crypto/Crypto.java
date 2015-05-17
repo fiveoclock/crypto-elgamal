2414bdea6ad14a5149d2941d36aaa8cc6161371d
Alex <alex@5-o-clock.net>
2015-05-17 18:36:33 +0200

    only keep elgmal
    Signed-off-by: Alex <alex@5-o-clock.net>

--- crypto/Crypto.java
+++ crypto/Crypto.java
@@ -17,15 +17,6 @@ public class Crypto {
 		
 		switch(args[0]) {
 		case "hash": 			c.checkArgs(3); c.hash(args[1], args[2]); break;
-		case "ecc": 			c.checkArgs(2); switch(args[1]) {
-			case "p192":			c.checkArgs(2); c.eccTest(args[1], args[2]); break;
-			default: 				printUsage("Unknown command in ecc");
-		} break;
-		case "cert": 			c.checkArgs(2); switch(args[1]) {
-			case "read": 			c.checkArgs(3); c.certRead(args[2]); break;
-			case "verify": 			c.checkArgs(4); c.verifyCert(args[2], args[3]); break;
-			default: 				printUsage("Unknown command in cert");
-			} break;
 		case "network":			c.checkArgs(2); switch(args[1]) {
 			case "server":			c.checkArgs(3);	c.netServer(args[2]); break;
 			case "client":			c.checkArgs(4);	c.netClient(args[2], args[3]); break;
@@ -40,18 +31,6 @@ public class Crypto {
 			case "service": 		c.checkArgs(4);	c.netStartElgamalService(args[2], args[3]); break;
 			default: 				printUsage("Unknown command in elgamal");
 			} break;
-		case "rsa": 			c.checkArgs(2); switch(args[1]) {
-			case "generate-keys": 	c.checkArgs(3);	c.rsaGenerateKeys(args[2]);	break;
-			case "encrypt":			c.checkArgs(4);	c.rsaEncrypt(args[2], args[3]); break;
-			case "decrypt": 		c.checkArgs(4); c.rsaDecrypt(args[2], args[3]); break;
-			default: 				printUsage("Unknown command in rsa");
-			} break;
-		case "dsa": 			c.checkArgs(2); switch(args[1]) {
-			case "generate-keys": 	c.checkArgs(3); c.dsaGenerateKeys(args[2]); break;
-			case "sign": 			c.checkArgs(4); c.dsaSign(args[2], args[3]); break;
-			case "verify": 			c.checkArgs(5);	c.dsaVerify(args[2], args[3], args[4], args[5]); break;
-			default: 				printUsage("Unknown command in dsa");
-			} break;
 		default: 					printUsage("Please specify command");
 		}
 	}
@@ -59,20 +38,6 @@ public class Crypto {
 	// Hashing
 	private void hash(String func, String text) {
 		System.out.println(lib.getHexHash(func, text.getBytes()));
-	}
-	
-	// Certificate functions
-	private void certRead(String filename) {
-		System.out.println( certH.getCertInfo((certH.readCert(filename))));
-	}
-	private void verifyCert(String file_ca, String file_cert) {
-		if ( certH.verifyCert(certH.readCert(file_ca), certH.readCert(file_cert)) ) {
-			System.out.println(certH.getCertInfo(certH.readCert(file_cert)));
-			System.out.println("The certificate "+file_cert+" was issued by the CA "+file_ca);
-		}
-		else {
-			System.out.println("The certificate "+file_cert+" was NOT issed by the CA "+file_ca);
-		}
 	}
 
 	// Elgamal
@@ -125,48 +90,6 @@ public class Crypto {
 	}	
 	
 	
-	// RSA
-	private void rsaGenerateKeys(String prefix) {
-		RSA rsa = new RSA();
-		rsa.generateKeys(2048);
-		rsa.saveKeys(prefix);
-	}
-	private void rsaEncrypt(String prefix, String message) {
-		RSA rsa = new RSA(prefix);
-		String c = rsa.encrypt(message);
-		System.out.println("Ciphertext (c): " + c);
-		
-		System.out.println("\nTesting decryption (m'): " + rsa.decrypt(c));
-	}
-	private void rsaDecrypt(String prefix, String cipher) {
-		RSA rsa = new RSA(prefix);
-		String m = rsa.decrypt(cipher);
-		System.out.println("Decrypted message (m'): " + m);
-	}
-
-	// DSA
-	private void dsaGenerateKeys(String prefix) {
-		DSA dsa = new DSA();
-		dsa.generateKeys();
-		dsa.saveKeys(prefix);
-	}
-	private void dsaSign(String prefix, String message) {
-		DSA dsa = new DSA(prefix);
-		Signature signature = dsa.sign(message.getBytes());
-		System.out.println("Signature \n r: " + signature.getR() + "\n s: " + signature.getS());
-	}
-	private void dsaVerify(String prefix, String r, String s, String message) {
-		DSA dsa = new DSA(prefix);
-		SignedMessage sm = new SignedMessage(message.getBytes(), new Signature(r, s));
-		if (dsa.verify(sm)) {
-			System.out.println("> Correct!");
-		}
-		else {
-			System.out.println("> Incorrect!");
-		}
-	}
-
-	
 	// Networking
 	private void netServer(String port) {
 		if (net.listen(Integer.parseInt(port)) ) {
@@ -202,38 +125,6 @@ public class Crypto {
 		}
 	}
 	
-	// ECC
-	private void eccTest(String curve, String ks) {
-		ECC ecc = new ECC();
-		BigInteger a, b, p;
-		ECPoint G = new ECPoint();
-		
-		if (curve.equalsIgnoreCase("p192")) {
-			// set domain parameters for curve p192 as specifed by:
-			// http://csrc.nist.gov/groups/ST/toolkit/documents/dss/NISTReCur.pdf
-			a = BigInteger.valueOf(-3);
-			b = new BigInteger("64210519e59c80e70fa7e9ab72243049feb8deecc146b9b1", 16);
-			p = new BigInteger("6277101735386680763835789423207666416083908700390324961279");
-			ecc = new ECC(a, b, p);
-			
-			BigInteger Gx = new BigInteger("188da80eb03090f67cbf20eb43a18800f4ff0afd82ff1012", 16);
-			BigInteger Gy = new BigInteger("07192b95ffc8da78631011ed6b24cdd573f977a11e794811", 16);
-			G = new ECPoint(Gx, Gy);
-		}
-		else {
-			printUsage("Unsupported curve: " + curve);
-		}
-		BigInteger k = new BigInteger(ks);
-		
-		System.out.println("Domain parameters: \n" + ecc.toString());
-		System.out.println("G: \n" + G.toString(16) );
-		System.out.println("K: " + k + " (in bits: " + k.toString(2) +")\n");
-		
-		// Scalar multiplication
-		// compare values with http://point-at-infinity.org/ecc/nisttv
-		ECPoint R = ecc.scalarMultiplication(G, k);
-		System.out.println(R.toString(16));
-	}
 
 	// general methods
 	private void checkArgs(int required) {
@@ -265,29 +156,12 @@ public class Crypto {
         .append("     decrypt [key-prefix] [input]                           \n")
         .append("       - decrypts the given input using the keys specified by key-prefix (you must run generate-keys first)\n")
         .append("                                                            \n")
-        .append("  cert:                                                     \n")
-		.append("     read [cert-file]                                       \n")
-		.append("       - reads a certificate and prints some information    \n")
-		.append("     verify [ca-file] [cert-file]                           \n")
-		.append("       - verifies if the certificate is derived from the specified CA certificate\n")
-		.append("                                                            \n")
-		.append("  dsa:                                                      \n")
-		.append("     generate-keys [key-prefix]                             \n")
-		.append("       - generates dsa keys and save them in files named dsa.prefix.*.key\n")
-		.append("     sign [key-prefix] [input]                              \n")
-		.append("       - signs the input message with the keys specified by input\n")
-		.append("     verify [key-prefix] [r] [s] [input]                    \n")
-		.append("       - verifies that the signature specified by r and s matches to the input message\n")
-		.append("                                                            \n")
 		.append("  network:                                                  \n")
 		.append("     server [port]                                          \n")
 		.append("       - starts a network hashing service; hashes incoming hashes and sends them back \n")
 		.append("     client [address] [port]                                \n")
 		.append("       - client for the hashing service                     \n")
 		.append("                                                            \n")
-		.append("  ecc:                                                      \n")
-		.append("     p192 [k]                                               \n")
-		.append("       - calculates point R on the NIST Curve P192          \n")
         .toString();
 		System.out.println(usage);
 	}
